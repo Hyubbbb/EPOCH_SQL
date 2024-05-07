163f96d2d288850ac86ec6f496749fdea06dab3d
tux2won <114200466+tux2won@users.noreply.github.com>
2024-05-07 22:04:52 +0900

    Update problem_3-1_solution_tw.sql

--- JOIN/Problem_3_1/problem_3-1_solution_tw.sql
+++ JOIN/Problem_3_1/problem_3-1_solution_tw.sql
@@ -1,16 +1,10 @@
 SELECT 
     f.FLAVOR
-FROM 
-    FIRST_HALF f
+FROM  FIRST_HALF f
 JOIN 
-    (
-        SELECT 
-            FLAVOR, 
-            SUM(TOTAL_ORDER) AS SUM_JULY_ORDER
-        FROM 
-            JULY
-        GROUP BY 
-            FLAVOR
+    (SELECT FLAVOR, SUM(TOTAL_ORDER) AS SUM_JULY_ORDER
+        FROM JULY
+        GROUP BY FLAVOR
     ) j ON f.FLAVOR = j.FLAVOR
 ORDER BY 
     (f.TOTAL_ORDER + j.SUM_JULY_ORDER) DESC
