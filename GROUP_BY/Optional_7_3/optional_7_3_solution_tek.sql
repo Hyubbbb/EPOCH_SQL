b8066cef34ec6cfe0078a722843b40f2d56f500d
benidjor <benidjor@gmail.com>
2024-06-04 00:29:12 +0900

    Optional 7-1 ~ 7-4

---
+++ GROUP_BY/Optional_7_3/optional_7_3_solution_tek.sql
@@ -0,0 +1,10 @@
+-- 프로그래머스 카테고리 별 도서 판매량 집계하기
+-- https://school.programmers.co.kr/learn/courses/30/lessons/144855
+
+SELECT B.CATEGORY, SUM(S.SALES) AS TOTAL_SALES
+FROM BOOK AS B
+LEFT JOIN BOOK_SALES AS S
+    ON B.BOOK_ID = S.BOOK_ID
+WHERE MONTH(S.SALES_DATE) = 1
+GROUP BY B.CATEGORY
+ORDER BY CATEGORY
